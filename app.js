eb99457d51a7287018ec40f55407a074f32fbe8e
KHUSH AGRAWAL <akhush2001@gmail.com>
2020-05-19 00:33:37 +0530

    added .gitignore and listening to coreect ports

--- app.js
+++ app.js
@@ -155,8 +155,13 @@ User.findOne({email:loggedinuser},function(err,foundUser){
   })
 });
 
+let port = process.env.PORT;
+if(port == null|| port == "")
+{
+ port=3000;
+}
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
