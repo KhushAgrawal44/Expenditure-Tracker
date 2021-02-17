0f1a1c078b7e6fe8be7a6458a136f1466533b203
KHUSH AGRAWAL <akhush2001@gmail.com>
2021-02-17 11:00:11 +0530

    GitHub Push

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose= require("mongoose");
 const md5= require("md5");
-
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,12 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-
-
-
 mongoose.connect("mongodb+srv://admin-khush:test123@cluster0-f7rnm.mongodb.net/moneyManagerDB", {useNewUrlParser: true,useUnifiedTopology:true});
-
-
 
 var expenditureSchema=new mongoose.Schema({
   date:String,
@@ -28,19 +21,16 @@ var expenditureSchema=new mongoose.Schema({
 })
 var Expenditure=mongoose.model("Expenditure",expenditureSchema);
 
-
 var userSchema=new mongoose.Schema({
   email:String,
   password:String,
- expenditures:[expenditureSchema],
- total:Number
+  expenditures:[expenditureSchema],
+  total:Number
 })
 var User = mongoose.model("User",userSchema);
 
-
-
 var expenditures=[];
-var lists=[];
+
 
 var total_expenditure=0;
 
@@ -58,31 +48,24 @@ app.get("/register",function(req,res){
   res.render("register");
 })
 
-
 app.get("/manager", function(req, res){
 Expenditure.find({},function(err,found){
   console.log(found.amount);
 })
 });
 
-
-
-
 app.post("/register",function(req,res){
-
   initial_total=0;
-
   User.findOne({email:req.body.username},function(err,foundUser){
     if(err){console.log(err);}
     else{
       if(!foundUser)
       {
         const user=new User({
-          email:req.body.username,
-          password:md5(req.body.password),
-          expenditures:expenditures,
+           email:req.body.username,
+           password:md5(req.body.password),
+           expenditures:expenditures,
            total:initial_total
-
         })
         user.save(function(err)
       {
@@ -97,10 +80,10 @@ app.post("/register",function(req,res){
       }
     }
   })
-
 })
 
 app.post("/login",function(req,res){
+  
   const login_email=req.body.username;
   const login_password=req.body.password;
 
@@ -114,14 +97,12 @@ app.post("/login",function(req,res){
         {
             res.render("manager",{emailid:foundUser.email,total_amount:foundUser.total,expenditures:foundUser.expenditures});
         }
-
       }
       else
       {
         res.redirect("/login");
       }
     }
-
   })
 })
 
@@ -131,25 +112,17 @@ app.post("/manager",function(req,res){
   var exp_date=req.body.date;
   var exp_amt=req.body.expenditure_amt;
   var exp_reason=req.body.reason;
-
   const loggedinuser=req.body.button;
-
   var expenditure=new Expenditure({
     date:exp_date,
     amount:exp_amt,
     reason:exp_reason
   })
 
-
-
 User.findOne({email:loggedinuser},function(err,foundUser){
-
-
     foundUser.expenditures.push(expenditure);
-
     foundUser.total=foundUser.total+expenditure.amount;
     foundUser.save();
-
     res.render("manager",{emailid:foundUser.email,expenditures:foundUser.expenditures,total_amount:foundUser.total})
 
   })
